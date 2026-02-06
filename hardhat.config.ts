e4e227b876a49059d8332d0e10e901f8fb421700
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-02-06 11:33:42 +0600

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    ethereum: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
